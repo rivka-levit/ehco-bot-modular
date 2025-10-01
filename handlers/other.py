35399c76c0c69a0a559be2dfaccb233ec36ab06c
rivka-yuliia <rivka.developer@gmail.com>
2025-10-01 15:09:35 +0300

    echo handler

---
+++ handlers/other.py
@@ -0,0 +1,15 @@
+"""
+Handler for random messages from users.
+"""
+
+from aiogram.types import Message
+
+from lexicon import LEXICON_RU
+
+
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer(text=LEXICON_RU['no_echo'])
