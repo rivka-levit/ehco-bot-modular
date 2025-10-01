ef350ccd5aede249b1ba5b61a8ae3bf78b4190fc
rivka-yuliia <rivka.developer@gmail.com>
2025-10-01 15:02:58 +0300

    user handlers

---
+++ handlers/user.py
@@ -0,0 +1,18 @@
+"""
+Handlers for basic communication with user via commands
+"""
+
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+
+from lexicon import LEXICON_RU
+
+
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
